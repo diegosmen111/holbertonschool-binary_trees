8e409afa5ba715a3b5c38953d36ed2d1d280e8a3
diegosmen111 <dbav_10@hotmail.com>
2023-05-15 23:54:05 -0700

    feat: fix task0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+  binary_tree_t *new =(binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+             if (new == NULL)
 		return (NULL);
 
 	new->n = value;
