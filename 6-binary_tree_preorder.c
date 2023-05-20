5051c930219d5e34951934272fe97ffacbb0bb25
Flagelo6482 <frankln6482@gmail.com>
2023-05-20 09:19:27 -0700

    feat: Update :rocket:

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Walk a binary tree with the pre-order method
+ *@tree: Pointer to the root node of the tree
+ *@func: Pointer to a function to call for each node
+ *
+ *Return: NULL or node value
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	print_num(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
+
+
+/**
+ * traverse_binary_tree_preorder - Funcion
+ *@tree: Tree
+ *
+ *Return: Void
+ */
+
+void traverse_binary_tree_preorder(const binary_tree_t *tree)
+{
+	binary_tree_preorder(tree, &print_num);
+}
