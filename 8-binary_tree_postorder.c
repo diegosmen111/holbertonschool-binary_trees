564cd923766d83c9deae4f32fb198a62a01dae5f
Flagelo6482 <frankln6482@gmail.com>
2023-05-20 10:03:28 -0700

    feat: Main file and executable :sparkles:

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Function that goes through a binary
+ *                         tree using post-order traversal
+ *@tree: Pointer to the root
+ *@func: Pointer to a function
+ *
+ *Return: Void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
